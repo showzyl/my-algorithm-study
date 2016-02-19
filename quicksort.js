049384ca59a641567ba16b381c146ee7b4da2a7d
showzyl <showzyl520@gmail.com>
2016-02-19 23:20:39 +0800

    First commit

---
+++ quicksort.js
@@ -0,0 +1,30 @@
+/**
+ * Created by showzyl on 16/2/19.
+ */
+
+'use strict';
+
+// curl -4 wttr.in/Beijing
+
+
+function quickSort(arr){
+  console.log(123);
+  if(arr.length<=1) return arr;
+
+}
+
+
+quickSort([]);
+
+
+
+
+
+
+
+
+
+
+
+
+
