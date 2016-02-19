12a5a59efe912ec37db04daf5ada9841d91213a1
showzyl <showzyl520@gmail.com>
2016-02-20 00:05:46 +0800

    first

--- quicksort.js
+++ quicksort.js
@@ -8,13 +8,30 @@
 
 
 function quickSort(arr){
-  console.log(123);
   if(arr.length<=1) return arr;
 
+  // chose middle
+  let mid = Math.floor(arr.length / 2);
+  //console.log(mid);
+  let pivot = arr.splice(mid, 1)[0];
+  var left = [];
+  var right = [];
+  //console.log(pivot);
+
+  for(var i = 0, len = arr.length; i<len;i++){
+    if(arr[i] < pivot){
+      left.push(arr[i]);
+    }else{
+      right.push(arr[i]);
+    }
+  }
+
+  return quickSort(left).concat( [ pivot ],quickSort(right) );
 }
 
 
-quickSort([]);
+console.log( quickSort([99,1,11,25,28,33]) );
+
 
 
 
