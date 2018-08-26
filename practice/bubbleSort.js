fd017baad87add3c290556db3ff00b4a49500dca
showzyl <showzyl520@gmail.com>
2018-08-27 03:45:36 +0800

    add bubble sort

--- practice/bubbleSort.js
+++ practice/bubbleSort.js
@@ -7,3 +7,51 @@
  **/
 
 'use strict'
+
+/**
+ * 冒泡排序
+ * @param arr
+ * @returns {*}
+ */
+function bubbleSort(arr) {
+  for (let i = arr.length - 1; i >= 1; i--) {
+    console.log(`i: `, i)
+    console.log(`=====================`)
+    for (let j = 1; j <= i; j++) {
+      console.log(`j: `, j)
+      if (arr[j - 1] > arr[j]) {
+        swap(arr, j - 1, j)
+      }
+    }
+    console.log(`=====================`)
+  }
+  return arr
+}
+
+function swap(arr, i, j) {
+  let tmp = arr[i]
+  arr[i] = arr[j]
+  arr[j] = tmp
+}
+
+console.log(`bubbleSort: `, bubbleSort([2, -10, -1, 5]))
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
