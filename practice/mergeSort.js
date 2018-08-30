27aa3191737759e0c4fe51dcec635c98694a2ea2
showzyl <showzyl520@gmail.com>
2018-08-30 11:14:50 +0800

    add merge sort

---
+++ practice/mergeSort.js
@@ -0,0 +1,36 @@
+/**
+ *
+ * Created by showzyl on 2018/8/30
+ *
+ *  [ 、 ( 、 + 、 -
+ *
+ **/
+
+'use strict'
+
+
+function mergeSort(A, l, r) {
+  if (r - l < 2) return
+  let m = Math.ceil((l + r) / 2)
+  mergeSort(A, l, m)
+  mergeSort(A, m, r)
+  merge(A, l, m, r)
+}
+
+function merge(A, l, m, r) {
+  const A1 = A.slice(l, m) // left
+  const A2 = A.slice(m, r) // right
+  A1.push(Number.MAX_SAFE_INTEGER)
+  A2.push(Number.MAX_SAFE_INTEGER)
+  for (let k = l, i = 0, j = 0; k < r; k++) {
+    if (A1[i] < A2[j]) {
+      A[k] = A1[i++]
+    } else {
+      A[k] = A2[j++]
+    }
+  }
+}
+
+let A = [20, -10, 100, 5, 2, 8]
+mergeSort(A, 0, A.length)
+console.log(`A: `, A)
