73c97ddc6efbe54856417d504f724bd88e7dd9b8
曾元黎 <showzyl520@gmail.com>
2018-09-20 00:48:30 +0800

    add  bucketSort

---
+++ practice/bucketSort.js
@@ -0,0 +1,37 @@
+
+const { insertSort } = require('./insertSort')
+// console.log(`insertSort: `, insertSort)
+/**
+ *
+ * @param A
+ * @param k 桶的数量
+ * @param S 每只桶的大小
+ */
+function bucketSort(A, k, S) {
+  const buckets = Array.from({length: k}, () => [])
+
+  // 放入桶中
+  for (let i = 0; i < A.length; i++) {
+    const index = ~~(A[i] / S)
+    buckets[index].push(A[i])
+  }
+
+  // 排序每只桶
+  for (let i = 0; i < buckets.length; i++) {
+    insertSort(buckets[i])
+  }
+
+  // 取出数据
+  return [].concat(...buckets)
+}
+
+const A = [29, 25, 3, 49, 9, 37, 21, 43]
+console.log(`A: `, bucketSort(A, 5, 10))
+
+
+
+
+
+
+
+
