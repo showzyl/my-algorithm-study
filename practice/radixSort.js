e9312579203145e8f83b906dc4c8cca8c9c467ca
曾元黎 <showzyl520@gmail.com>
2018-09-21 13:47:03 +0800

    add radixsort

---
+++ practice/radixSort.js
@@ -0,0 +1,57 @@
+/**
+ *
+ * Created by showzyl on 2018/9/14
+ *
+ *  [ 、 ( 、 + 、 -
+ *
+ **/
+
+/**
+ * 基数排序 radix
+ * @param A
+ * @returns {any[]}
+ */
+function radixSort(A) {
+  const max = Math.max(...A)
+  const buckets = Array.from({length: 10}, () => [])
+  // 有效数位
+  let m = 1
+  while (m < max){
+    // 将数组放入桶中
+    A.forEach(number => {
+      const digit = ~~ ( (number % (m * 10)) / m )
+      buckets[digit].push(number)
+    })
+    // 从桶中取出元素
+    let j = 0
+    buckets.forEach(bucket => {
+      while (bucket.length > 0) {
+        A[j++] = bucket.shift()
+      }
+    })
+    // 下一个位置
+    m *= 10
+  }
+}
+
+const A = [10, 200, 13, 12, 7, 88, 91, 24]
+radixSort(A)
+console.log(`A: `, A)
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
