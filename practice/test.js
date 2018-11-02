f5c36cb4d6320c71c72e3c60c58e2b8aff777cf9
曾元黎 <showzyl520@gmail.com>
2018-11-02 23:09:37 +0800

    add render func

--- practice/test.js
+++ practice/test.js
@@ -844,21 +844,910 @@
 // console.log(`res: `, insertSort(arr))
 
 
-function sort(arr) {
-  let cache = {}
-  arr.reduce((pre, cur) => {
-    if (cache[cur]) {
-      cache[cur]++
-    } else {
-      cache[cur] = 1
-    }
-    console.log(`cur: `, cur)
-  }, cache)
-  console.log(`cache: `, cache)
+// function sort(arr) {
+//   let cache = {}
+//   arr.reduce((pre, cur) => {
+//     if (cache[cur]) {
+//       cache[cur]++
+//     } else {
+//       cache[cur] = 1
+//     }
+//     console.log(`cur: `, cur)
+//   }, cache)
+//   console.log(`cache: `, cache)
+// }
+//
+// let arr = [1, 2, 3, 1, 1, 0, 5, 5, 5, 5, 7]
+// sort(arr) // [5,5,5,5,1,1,1,0,2,3,7]
+
+
+// /**
+//  *
+//  * @param str 全集
+//  * @param decisions 已经选择的字符
+//  */
+// function permutation(str, decisions) {
+//   if(decisions中包含了str全部字符) return
+//   let r = []
+//   for (c in str中所有不在decisions中的字符) {
+//     r = r.concat(permutation(str, decisions+c))
+//   }
+//   return r
+// }
+//
+
+// /**
+//  *
+//  * @param S 数组，需要求组合的集合
+//  * @param k 取出元素个数
+//  */
+// function combination(S, k) {
+//   if(k === 0 || S.length === k) return [ S.slice(0, k) ]
+//   const [first, ...others] = S
+//   let r = []
+//   r = r.concat( combination(others, k-1).map(c => [first, ...c]) )
+//   r = r.concat( combination(others, k) )
+//
+//   return r
+// }
+//
+//
+// const S = ['A', 'B', 'C', 'D']
+// console.log(`S: `, combination(S, 2))
+
+
+// console.log(`a: `, 123)
+
+// function insertSort(arr) {
+//   for (let i = 1; i < arr.length; i++) {
+//     insert(arr, i, arr[i])
+//   }
+//   return arr
+// }
+//
+// function insert(arr, i, x) {
+//   // p 指针指向下一个需要比较的元素
+//   // p + 1 指向空位
+//   let p = i - 1
+//   while (p >= 0 && arr[p] > x) {
+//     arr[p + 1] = arr[p]
+//     p--
+//   }
+//   arr[p + 1] = x
+// }
+
+
+// function insert(arr, i, x){
+//   let p = i - 1
+//   while(p >= 0 && arr[p] > x){
+//     arr[p+1] = arr[p]
+//     p--
+//   }
+//   arr[p+1] = x
+// }
+//
+// function insertSort(arr){
+//   for (let i = 1; i < arr.length; i++) {
+//     insert(arr, i, arr[i])
+//   }
+//   return arr
+// }
+
+// function mergeSort(arr, l, r) {
+//   if (r - l < 2) return
+//   let m = Math.ceil((l + r) / 2)
+//   console.log(`m: `, m)
+//   mergeSort(arr, l, m)
+//   mergeSort(arr, m, r)
+//   merge(arr, l, m, r)
+// }
+//
+// function merge(arr, l, m, r) {
+//   const a1 = arr.slice(l, m)
+//   const a2 = arr.slice(m, r)
+//   a1.push(Number.MAX_SAFE_INTEGER)
+//   a2.push(Number.MAX_SAFE_INTEGER)
+//
+//   for (let k = l, i = 0, j = 0; k < r; k++) {
+//     if(a1[i] < a2[j]){
+//       arr[k] = a1[i++]
+//     }else{
+//       arr[k] = a2[j++]
+//     }
+//   }
+// }
+//
+//
+// let arr = [10, 5, 3, 7]
+// mergeSort(arr, 0, arr.length)
+// console.log(`res: `, arr)
+
+
+// function bubbleSort(arr) {
+//   for (let i = arr.length-1; i >= 1; i--) {
+//     for (let j = 1; j <= i; j++) {
+//       if(arr[j] < arr[j-1]){
+//         swap(arr, j, j-1)
+//       }
+//     }
+//   }
+//   return arr
+// }
+//
+// function swap(arr, i, j) {
+//   // [arr[i], arr[j]] = [arr[j], arr[i]]
+//   let t = arr[i]
+//   arr[i] = arr[j]
+//   arr[j] = t
+// }
+
+
+// function bubbleSort(arr){
+//   for (let i = arr.length - 1; i >= 1; i--) {
+//     for (let j = 1; j < i; j++) {
+//       console.log(`j: `, j)
+//       console.log(`j - 1: `, j - 1)
+//       if(arr[j] < arr[j-1]){
+//         swap(arr, j, j-1)
+//       }
+//     }
+//     console.log(`==================`)
+//   }
+//   return arr
+// }
+//
+// function swap(arr, i, j){
+//   let t = arr[i]
+//   arr[i] = arr[j]
+//   arr[j] = t
+// }
+
+// function swap(arr, i, j) {
+//   [arr[i], arr[j]] = [arr[j], arr[i]]
+// }
+//
+// function partition(a, l, r) {
+//   const pivot = a[r - 1]
+//   let i = l
+//   let j = r - 1
+//   while (i !== j) {
+//     if (a[i] < pivot) {
+//       i++
+//     } else {
+//       swap(a, i, --j)
+//     }
+//   }
+//   swap(a, j, r-1)
+//   return j
+// }
+//
+// function quickSort(a, l, r) {
+//   if (r - l <= 1) return
+//   const p = partition(a, l, r)
+//   quickSort(a, l, p)
+//   quickSort(a, p, r)
+// }
+//
+//
+// // console.log(`res: `, bubbleSort([2, -10, -1, 5]))
+// // console.log(`insert res: `, insertSort([2, -10, -1, 5]))
+// // let a = [2, -10, -1, 5]
+// // mergeSort(a, 0, a.length)
+// // console.log(`insert res: `, a)
+//
+// let a = [2, -10, -1, 5]
+// quickSort(a, 0, a.length)
+// console.log(`insert res: `, a)
+
+
+// i, j, k代表碟子，k最小，i最大
+// A, B, C是三个位置
+
+function solveHanoiTower(disks, from, to, use) {
+  // console.log(`disks: `, disks)
+  // console.log(`from: `, from)
+  // console.log(`to: `, to)
+  // console.log(`use: `, use)
+  move(disks, 0, disks.length, from, to, use)
 }
 
-let arr = [1, 2, 3, 1, 1, 0, 5, 5, 5, 5, 7]
-sort(arr) // [5,5,5,5,1,1,1,0,2,3,7]
+// function move(disks, k, n, from, to, use) {
+//   console.log(`${disks[n-1]}: `, `${from}->${use}`)
+//   console.log(`${disks[n-2]}: `, `${from}->${to}`)
+
+//   console.log(`${disks[n-1]}: `, `${use}->${to}`)
+// }
+
+// function move(disks, k, n, from, to, use) {
+//   console.log(`${disks[n-1]}: `, `${from}->${to}`)
+//   console.log(`${disks[n-2]}: `, `${from}->${use}`)
+//
+//   console.log(`${disks[n-1]}: `, `${to}->${use}`)
+//   console.log(`${disks[n-3]}: `, `${from}->${to}`)
+//
+//   console.log(`${disks[n-1]}: `, `${use}->${from}`)
+//   console.log(`${disks[n-2]}: `, `${use}->${to}`)
+//
+//   console.log(`${disks[n-1]}: `, `${from}->${to}`)
+// }
+
+// function move(disks, k, n, from, to, use) {
+//   if(n === 1) {
+//     console.log(`${disks[k]}: ${from}->${to}`)
+//     return
+//   }
+//   move(disks, k+1, n-1, from, use, to)
+//   console.log(`${disks[k]}: ${from}->${to}`)
+//   move(disks, k+1, n-1, use, to, from)
+// }
+//
+// solveHanoiTower(['i', 'j', 'k'], 'A', 'B', 'C')
+
+// k A->B
+// j A->C
+// k B->C
+// i A->B
+// k C->A
+// j C->B
+// k A->B
+
+
+// permutation('abc') // [ 'abc', 'acb', 'bac', 'bca', 'cab', 'cba' ]
+
+// function permutation(str, select = []) {
+//   // console.log(`str: `, str)
+//   console.log(`select: `, select)
+//   if (select.length === str.length) {
+//     console.log(`here: `, select.map(i => str[i]).join(``))
+//     return select.map(i => str[i]).join(``)
+//   }
+//
+//   let r = []
+//   for (let i = 0; i < str.length; i++) {
+//     if (select.indexOf(i) === -1) {
+//       // console.log(`select: `, select)
+//       // console.log(`before r: `, r)
+//       r = r.concat(permutation(str, select.concat(i)))
+//       // console.log(`after r: `, r)
+//     }
+//   }
+//   console.log(`===================`)
+//   return r
+// }
+//
+//
+// console.log(`permutation: `, permutation('abc'))
+
+
+// function findSubsets(S, decisions = []) {
+//   // 所有决策都已经完成
+//   if(S.length === decisions.length){
+//     // 返回结果
+//     return decisions
+//   }
+//   let r = []
+//   r = r.concat(findSubsets(S, decisions.concat(true)))
+//   r = r.concat(findSubsets(S, decisions.concat(false)))
+//   return r
+// }
+//
+// console.log(`findSubsets: `, findSubsets('abc'))
+//
+//
+// 全排列问题抽象
+// function permutation(str, decisions) {
+//   // 所有决策都已经完成
+//   if(decisions包含str全部字符){
+//     // 返回结果
+//     return decisions
+//   }
+//
+//   let r = []
+//   for(c in str中所有不在decisions中的字符){
+//     r = r.concat(permutation(str, decisions + c))
+//   }
+//
+//   return r
+// }
+
+
+// function mergeSort(a, l = 0, r = a.length) {
+//   if (r - l < 2) return
+//   let m = Math.ceil((l + r) / 2)
+//   mergeSort(a, l, m)
+//   mergeSort(a, m, r)
+//   merge(a, l, m, r)
+//   return a
+// }
+//
+// function merge(a, l, m, r) {
+//   const a1 = a.slice(l, m)
+//   const a2 = a.slice(m, r)
+//   a1.push(Number.MAX_SAFE_INTEGER)
+//   a2.push(Number.MAX_SAFE_INTEGER)
+//
+//   for (let i = 0, j = 0, k = l; k < r; k++) {
+//     if (a1[i] < a2[j]) {
+//       a[k] = a1[i++]
+//     } else {
+//       a[k] = a2[j++]
+//     }
+//   }
+// }
+//
+// const a = [10, 7, 5, 2, 4]
+// console.log(`a: `, mergeSort(a, 0, a.length))
+
+// function quickSort(arr) {
+//
+// }
+
+
+// function swap(a, i, j) {
+//   let t = a[i]
+//   a[i] = a[j]
+//   a[j] = t
+// }
+//
+// function partition(a, l, r) {
+//   const pivot = a[r - 1]
+//   let i = l
+//   let j = r - 1
+//   // 小于中心点范围 [ l, i )
+//   // 未确认范围 [ i, j )
+//   // 大于中心点范围 [ j, r-1 )
+//
+//   while (i !== j) {
+//     if (a[i] <= pivot) {
+//       i++
+//     } else {
+//       swap(a, i, --j)
+//     }
+//   }
+//   swap(a, j, r - 1)
+//   return j
+// }
+//
+//
+// function quickSort(a, l = 0, r = a.length) {
+//   if (r - l <= 1) return
+//   const p = partition(a, l, r)
+//   quickSort(a, l, p)
+//   quickSort(a, p + 1, r)
+//   return a
+// }
+//
+// const a = [10, 5, 8, 4, 2]
+// console.log(`a: `, quickSort(a, 0, a.length))
+
+
+// function permutation(str, select = []) {
+//   if (select.length === str.length) {
+//     return select.map(i => str[i]).join(``)
+//   }
+//   let r = []
+//   for (let i = 0; i < str.length; i++) {
+//     if (select.indexOf(i) === -1) {
+//       r = r.concat(permutation(str, select.concat(i)))
+//     }
+//   }
+//   return r
+// }
+//
+// console.log(`permutation: `, permutation('abc'))
+
+
+// function findSubsets(str, selects = []) {
+//   if(str.length === selects.length){
+//     console.log(`selects: `, selects)
+//     let s = selects.map((item, i) => (item ? str[i] : '')).join('')
+//     console.log(`s: `, s)
+//     console.log(`======================`)
+//     return [s]
+//   }
+//   let r = []
+//   r = r.concat(findSubsets(str, selects.concat(true)))
+//   r = r.concat(findSubsets(str, selects.concat(false)))
+//   console.log(`r: `, r)
+//   console.log(`======================`)
+//   return r
+// }
+//
+// console.log(`findSubsets: `, findSubsets('abc'))
+
+
+// function flat(arr) {
+//   return [].concat(...arr.map(x => {
+//     if (Array.isArray(x)) {
+//       return flat(x)
+//     } else {
+//       return x
+//     }
+//   }))
+// }
+//
+// console.log(`flat: `, flat([1,[2,'a',[5,6]],8]))
+
+// 递归反转数组
+// function reverse(a, i, j) {
+//   if( parseInt(a.length / 2) === i ){
+//     // console.log(`a: `, a)
+//     a = a.filter(item => item !== undefined)
+//     console.log(`aa: `, a)
+//     return a
+//   }
+//   swap(a, i, j)
+//   reverse(a, ++i, --j)
+// }
+//
+// function swap(a, i, j) {
+//   let t = a[i]
+//   a[i] = a[j]
+//   a[j] = t
+// }
+//
+// let a = [1,2,3]
+// reverse(a, 0, a.length)
+// console.log(`a: `, a)
+
+
+// function reverse(arr) {
+//   for (let i = 0; i < arr.length / 2; i++) {
+//     console.log(`i: `, i)
+//     let t = arr[i]
+//     arr[i] = arr[arr.length - i - 1]
+//     arr[arr.length - i - 1] = t
+//   }
+// }
+
+// let a = [1,2,3]
+// reverse(a)
+// console.log(`a: `, a)
+
+
+// function reverse(a, i) {
+//   if(i === parseInt(a.length / 2)){
+//     return a
+//   }
+//   let t = a[i]
+//   a[i] = a[a.length - i - 1]
+//   a[a.length - i - 1] = t
+//   reverse(a, ++i)
+// }
+//
+// let a = [1,2,3]
+// reverse(a, 0)
+// console.log(`a: `, a)
+
+// solution(10) // 23
+// solution(16) // 60
+
+// const A = [1, 2, 2, 3, 6, 3]
+// countSort(A)
+
+// console.log(`insertSort: `, insertSort(A))
+// console.log(`bubbleSort: `, bubbleSort(A))
+// console.log(`mergeSort: `, mergeSort(A))
+// console.log(`countSort: `, countSort(A))
+// console.log(`quickSort: `, quickSort(A))
+
+// class LinkList {
+//   constructor() {
+//     this.head = null
+//   }
+//
+//   insert(node) {
+//     if (this.head !== null) {
+//       node.text = this.head
+//     }
+//     this.head = node
+//   }
+//
+//   find(node) {
+//     let p = this.head
+//     while (p && p !== node) {
+//       p = p.next
+//     }
+//     return p
+//   }
+//
+// }
+//
+
+
+function ListNode(key) {
+  this.key = key
+  this.next = null
+}
+
+
+// class LinkedList {
+//   constructor() {
+//     this.head = null
+//   }
+//
+//   // O(1)
+//   insert(node) {
+//     if (this.head !== null) {
+//       node.next = this.head
+//     }
+//     this.head = node
+//   }
+//
+//   find(node) {
+//     // 遍历指针
+//     let p = this.head
+//     // 循环遍历直到找到和`node`相同的节点
+//     while (p && p !== node) {
+//       p = p.next
+//     }
+//     return p
+//   }
+//
+//   reverse(p = this.head) {
+//     if (p.next) {
+//       this.reverse(p.next)
+//       p.next.next = p
+//       p.next = null
+//     } else {
+//       this.head = p
+//     }
+//   }
+//
+//   print() {
+//     console.log(`LinkedList: `, this)
+//   }
+//
+// }
+//
+// const list = new LinkedList()
+//
+// const node = new ListNode(1)
+// const node2 = new ListNode(2)
+// const node3 = new ListNode(3)
+//
+// // list.head = node
+// list.insert(node2)
+// list.insert(node3)
+// list.insert(node)
+//
+// // node2.next = node
+//
+// console.log(`list: `, JSON.stringify(list))
+// // console.log(`node: `, node)
+
+
+// let A = []
+
+// function sum(A) {
+//   let r = 0
+//   for (let i = 0; i < A.length; i++) {
+//     r += A[i]
+//   }
+//   return r
+// }
+
+// function sum(A, i = 0, r = 0){
+//   if(!A.length) return
+//   if(i === A.length) return r
+//   return sum(A, i+1) + A[i] + r
+// }
+//
+// console.log(`sum: `, sum(A))
+
+
+// function permutation(str, select = []) {
+//   if (select.length === str.length) {
+//     return select.map(i => str[i]).join(``)
+//   }
+//   let r = []
+//   for (let i = 0; i < str.length; i++) {
+//     if (select.indexOf(i) === -1) {
+//       r = r.concat(permutation(str, select.concat(i)))
+//     }
+//   }
+//   return r
+// }
+//
+// console.log(`permutation: `, permutation('abc'))
+//
+
+
+// class MaxHeap {
+//   constructor(data, Max = 10000) {
+//     this.list = new Array(Max)
+//     for (let i = 0; i < data.length; i++) {
+//       this.list[i] = data[i]
+//     }
+//     this.heapSize = data.length
+//     this.build()
+//   }
+//
+//   build() {
+//     let i = Math.floor(this.heapSize / 2) - 1
+//     while (i >= 0) {
+//       this.maxHeapify(i--)
+//     }
+//   }
+//
+//   maxHeapify(){
+//
+//   }
+//
+// }
+
+
+// let t = {
+//   n: {
+//     k: 1,
+//     y: 2,
+//     d: {
+//       a: 1,
+//       b: 2
+//     }
+//   },
+//   b: 3
+// }
+//
+// function isObj(p) {
+//   return Object.prototype.toString.call(p) === '[object Object]'
+// }
+//
+// function zip(t) {
+//   console.log(`t: `, t)
+//   let o = {}
+//
+//   for (let k in t) {
+//     // console.log(`k: `, k)
+//     // console.log(`k: `, t[k])
+//     if(isObj(t[k])){
+//       // 是对象
+//
+//     }else {
+//       // 不是
+//       o[k] = t[k]
+//     }
+//   }
+//
+//   return o
+// }
+//
+// console.log(`zip(t): `, zip(t)); // 返回以下结果
+
+// {
+//   b       : 3,
+//   'n.k'   : 1,
+//   'n.y'   : 2,
+//   'n.d.a' : 1,
+//   'n.d.b' : 2,
+// }
+
+
+// const A = [2, 100, 5, -10, 6]
+// const B = [2, 100, 5, -10, 6]
+// const C = [2, 100, 5, -10, 6]
+
+
+// function merge(A, l, m, r) {
+//
+// }
+//
+// console.log(`C: `, merge(C, 0, 3, C.length))
+
+
+// const A = [10, 1, 0, 5, 2, 8]
+// function countSort(A) {
+//
+// }
+//
+// console.log(`countSort(A): `, countSort(A))
+//
+
+// // 合并两个有序数组
+// let A = [1,5,10]
+// let B = [2,4,11]
+//
+// let C = A.concat(B)
+// console.log(`C: `, C)
+// function merge(A, l, m, r) {
+//
+// }
+//
+// console.log(`merge: `, merge(C, 0, 3, C.length))
+
+// let A = [1,5,10]
+// let B = [2,4,6]
+//
+// let C = A.concat(B)
+//
+// function swap(A, i, j) {
+//   [A[i], A[j]] = [A[j], A[i]]
+// }
+//
+// function partition(A, l, r) {
+//
+// }
+//
+// // let A = [10, 80, 30, 90, 40, 50, 70]
+// console.log(C, partition(C, 0, 6))
+
+
+// let A = [10, 80, 30, 90, 40, 50, 70]
+// let A = [1, 10, 40, 80, 200]
+
+
+// function binarySearch(A, x) {
+//   let l = 0
+//   let r = A.length
+//   let guess
+//
+//   while (l <= r) {
+//     guess = Math.floor((l + r) / 2)
+//     if (A[guess] === x) {
+//       return guess
+//     } else if (A[guess] > x) {
+//       r = guess - 1
+//     } else {
+//       l = guess + 1
+//     }
+//   }
+// }
+//
+// console.log(`binarySearch(A, x): `, binarySearch(A, 10))
+
+
+// '2018-10-07T11:48:47 Asia/zh-cn'  用正则取出 ['2018', '10', '07', '11', '48', '47']
+
+
+// const man = {
+//   name: 'jsCoder',
+//   age: 22
+// }
+
+// // 补全代码
+// const proxy = new Proxy(...)
+//
+// proxy.name // jsCoder
+// proxy.age // 22
+// proxy.location // Property 'location' does not exist
+
+// let obj = new Proxy({}, {
+//   get: function (target, key, receiver) {
+//     console.log(`getting ${key}!`);
+//     return Reflect.get(target, key, receiver);
+//   },
+//   set: function (target, key, value, receiver) {
+//     console.log(`setting ${key}!`);
+//     return Reflect.set(target, key, value, receiver);
+//   }
+// })
+//
+// obj.count = 1
+// //  setting count!
+// obj.count++
+
+
+// let domNode = {
+//   tagName: 'ul',
+//   props: {class: 'list'},
+//   childen: [{
+//     tagName: 'li',
+//     childen: ['toutiao']
+//   },{
+//     tagName: 'li',
+//     childen: ['douyin']
+//   }]
+// }
+//
+// function render() {
+//
+// }
+
+function render(data) {
+  let el = document.createElement(data.tagName)
+
+  if(data.props){
+    for (let k in data.props) {
+      el.setAttribute(k, data.props[k])
+    }
+  }
+
+  data.childen.forEach(item => {
+    let child = item.childen.length > 1 ? render(createDom(item)) : document.createTextNode(item)
+    el.appendChild(child)
+  })
+
+  console.log(`el: `, el)
+  return el
+}
+
+function createDom(data) {
+  let el = document.createElement(data.tagName)
+
+  if(data.props){
+    for (let k in data.props) {
+      el.setAttribute(k, data.props[k])
+    }
+  }
+
+  return el
+}
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
+
 
 
 
