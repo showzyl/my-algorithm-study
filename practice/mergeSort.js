7437d027e44d3646b38e1a1eaf2e7461e8e14e29
曾元黎 <showzyl520@gmail.com>
2019-03-08 16:19:42 +0800

    feat: add merge.js

--- practice/mergeSort.js
+++ practice/mergeSort.js
@@ -28,12 +28,37 @@ function merge(A, l, m, r) {
   A1.push(Number.MAX_SAFE_INTEGER)
   A2.push(Number.MAX_SAFE_INTEGER)
   for (let k = l, i = 0, j = 0; k < r; k++) {
+    // 循环不变式
+    // k 下一个写入位置
+    // i: A1 中回写位置
+    // j: A2 中回写位置
     if (A1[i] < A2[j]) {
       A[k] = A1[i++]
     } else {
       A[k] = A2[j++]
     }
   }
+}
+
+// 普通写法
+function merge2(a, l, m, r) {
+  let a1 = a.slice(l, m)
+  let a2 = a.slice(m, r)
+  let i = 0, j = 0, k = l
+  while (i < a1.length && j < a2.length) {
+    if (a1[i] < a2[j]) {
+      a[k++] = a1[i++]
+    } else {
+      a[k++] = a2[j++]
+    }
+  }
+  while (i < a1.length) {
+    a[k++] = a1[i++]
+  }
+  while (j < a2.length) {
+    a[k++] = a2[j++]
+  }
+  console.log(`res: `, a)
 }
 
 let A = [20, -10, 100, 5, 2, 8]
