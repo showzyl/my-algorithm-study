c59f820eed30410a6580a0f7e1632c7e7f02ed0f
曾元黎 <showzyl520@gmail.com>
2019-03-21 23:35:03 +0800

    add feat: add scriptoj 61

--- algor-practice-daily/8.js
+++ algor-practice-daily/8.js
@@ -1,3 +1,10 @@
+// 写一个函数solution，求比一个数字n小的所有3和5的整数倍数和。
+// 比如10，比它小的3、5整数倍数有： 3,5,6,9， 所以和为23。 比如16， 比它小的3，5整数倍数有： 3,5,6,9,10,12,15，所以和为60（15只计算1次）
+//
+// 示例
+// solution(10) // 23
+// solution(16) // 60
+
 function solution(num) {
   let map = new Map()
   for (let i = 1; i < num; i++) {
@@ -12,6 +19,13 @@ function solution(num) {
   })
   return res
 }
+
+// function solution(number){
+//   if(number < 0) return 0
+//   return [...Array(number).keys()]
+//     .filter(n => n % 3 === 0 || n % 5 === 0)
+//     .reduce((a, b) => a + b,0)
+// }
 
 console.log(`res1: `, solution(10))
 console.log(`res2: `, solution(16))
---
+++ scriptoj/61.监听数组变化.js
@@ -0,0 +1,34 @@
+// http://scriptoj.mangojuice.top/problems/61
+
+// 在前端的 MVVM 框架当中，我们经常需要监听数据的变化，而数组是需要监听的重要对象。
+// 请你完成 ObserverableArray，它的实例和普通的数组实例功能相同，但是当调用：
+// 这些方法的时候，除了执行相同的操作，还会把方法名打印出来。 例如：
+// const arr = new ObserverableArray()
+//
+// arr.push('Good') // => 打印 'push'，a 变成了 ['Good']
+// 注意，你不能修改 Array 的 prototype。还有函数 return 的值和原生的操作保持一致。
+
+
+"use strict"
+
+function ObserverableArray(...q) {
+  // console.log(`q: `, q)
+  const arrMethodsName = ['push', 'pop', 'shift', 'unshift', 'splice', 'sort', 'reverse']
+  return new Proxy([...q], {
+    get(target, property){
+      if(arrMethodsName.includes(property)){
+        return function (...p){
+          // console.log(property);
+          console.log(`p: `, p);
+          return target[property].apply(this, p);
+        }
+      }
+    }
+  })
+}
+
+const arr = new ObserverableArray('c', 'd', 'e')
+arr.push('Good', 'test', 'a', 'v')
+
+
+
