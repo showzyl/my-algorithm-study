d34a2ad9aa49a91a846227e917a2ba705f344edb
show <showzyl520@gmail.com>
2019-04-18 21:51:18 +0800

    feat: add daily 22

---
+++ algor-practice-daily/22.js
@@ -0,0 +1,14 @@
+'use strict'
+
+// 22. 2018年9月3日题目
+// 封装一个函数sequence对一个无限序列求值。函数接收一个lambda表达式代表一个无限序列，然后提供两个操作take和takeWhile
+
+
+// sequence( n => n * n ).take(5) // [0, 1, 4, 9, 16]
+// sequence( n => n * 4 ).takeWhile( n => n < 20 ) // [0, 4, 8, 12, 16]
+
+
+
+
+
+
