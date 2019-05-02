909396051f33731a9695a679eabdadcbf7ac0beb
show <showzyl520@gmail.com>
2019-05-03 01:52:54 +0800

    feat: add daily 39...

---
+++ scriptoj/39.不用循环生成数组.js
@@ -0,0 +1,11 @@
+// http://scriptoj.mangojuice.top/problems/39
+
+// 完成 arrWithoutLoop 函数，它会被传入一个整数 n 作为参数，
+// 返回一个长度为 n 的数组，数组中每个元素的值等于它的下标。arrWithoutLoop 中不能使用循环控制结构。
+
+const arrWithoutLoop = (n) => {
+  return Array(n).fill(0).map((item, i) => i )
+}
+
+console.log(`res: `, arrWithoutLoop(10))
+
