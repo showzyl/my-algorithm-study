1907f88e6227382d96439a652f3fd6738861569f
show <showzyl520@gmail.com>
2019-05-11 16:03:05 +0800

    fixed: ratInMaze error...

--- practice/ratInMaze.js
+++ practice/ratInMaze.js
@@ -26,28 +26,36 @@ const maze = [
 function ratInMaze(
   maze,
   pos = [0, 0],
-  path = [[...path]],
+  path = [[...pos]],
   transverse = []) {
 
   const [x, y] = pos
   // 基础条件
-  if (maze[x][y] === 2) return path
+  if (maze[x][y] === 2) {
+    console.log(`: `, )
+    return path
+  }
   // 记录走过当前位置
   transverse[x * maze.length + y] = 1
+  console.log(`transverse: `, transverse)
 
   // 找到可能的选择
   const choices = [
     [x + 1, y], [x - 1, y],
     [x, y + 1], [x, y - 1]
-  ].filter((x, y) => {
+  ].filter(([x, y]) => {
     return x >= 0 && y >= 0 && x < maze.length && y < maze[0].length
       && (maze[x][y] !== 1) && !transverse[x * maze.length + y]
   })
 
   // 对每个选择,继续递归,寻找路径
   for(let[x, y] of choices){
-    const p = ratInMaze(maze, [x, y], path.concat([x, y]), transverse)
+    const p = ratInMaze(maze, [x, y], path.concat([[x, y]]), transverse)
     if(p) return p
   }
 
 }
+
+console.log(`res: `, ratInMaze(maze))
+
+
