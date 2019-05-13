109a0d66037746a95042bb5c74ecf527f5a2bba9
show <showzyl520@gmail.com>
2019-05-13 16:11:02 +0800

    fixed: modify dfs...

--- practice/dfs.js
+++ practice/dfs.js
@@ -5,11 +5,11 @@
 
 function dfs(node) {
   const stack = [node]
-  while (stack.length > 0){
+  while (stack.length > 0) {
     const first = stack.shift()
     console.log(`first key: `, first.key)
     // 子节点压栈
-    first.childen.slice().reverse().forEach(
+    first.children.slice().reverse().forEach(
       child => stack.unshift(child)
     )
   }
@@ -20,4 +20,37 @@ function dfs(node) {
 //   console.log(`node.key: `, node.key)
 //   node.childen.forEach(dfs)
 // }
+
+
+function Node(key) {
+  this.children = []
+  this.key = key
+}
+
+const n1 = new Node('1')
+const n2 = new Node('2')
+const n3 = new Node('3')
+const n4 = new Node('4')
+const n5 = new Node('5')
+const n6 = new Node('6')
+
+
+n1.children.push(n2)
+n1.children.push(n5)
+n2.children.push(n3)
+n2.children.push(n4)
+n5.children.push(n6)
+
+console.log(`n1: `, JSON.stringify(n1))
+
+
+dfs(n1)
+
+
+
+
+
+
+
+
 
